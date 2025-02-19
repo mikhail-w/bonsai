0a85d675163af3b10b49fca93f80401d2d308309
mikhail <mikhail.waddell@gmail.com>
2025-02-19 14:11:29 -0500

    make navigation component modular

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import { Outlet } from 'react-router-dom';
 import ScrollToTop from './components/ScrollToTop';
-import Navigation from './components/Navigation';
-// import Navigation from './components/Navigation/Navigation';
+import Navigation from './components/Navigation/Navigation';
 import Footer from './components/Footer';
 
 import './index.css';
